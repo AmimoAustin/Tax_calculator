548a3704c69baa57724e1c15ef96e240e278ae2e
AmimoAustin <austinamimo254@gmail.com>
2021-08-16 17:14:05 +0300

    updated

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
-import tax_calculator as tc
 
-while True:
+import Worker as pc
+
+workers = []
+counter = 0
+
+
+def all_people():
+    if len(workers) > 0:
+        print("|No.| Name | Age | Gross Pay | Net Pay |")
+        print("|---|------|-----|-----------|---------|")
+        for worker in workers:
+            print(worker.toString(workers.index(worker) + 1))
+    else:
+        print("No Workers")
+
+
+while counter < 2:
     try:
         name = str(input("Enter your name: \n"))
         try:
             age = int(input("Enter your age: \n"))
             gross_pay = int(input("Enter your pay: \n"))
-            net_pay = tc.total_calc_tax(gross_pay, age)
-            print(name, "your take away home salary is ,", "Ksh:", net_pay)
+            person = pc.Person(
+                name,
+                age,
+                gross_pay,
+            )
+            workers.append(person)
+            counter += 1
         except ValueError:
             print("Invalid Data")
     except KeyboardInterrupt:
         print('The program was stopped by the user')
+        all_people()
         exit()
+all_people()
